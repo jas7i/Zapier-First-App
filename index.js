244a0fa54342d306912137079fdbbbbfe7e53d79
JAS7I <wjas601@gmail.com>
2023-05-30 11:09:59 +0530

    First app build in zapier using CLI

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ module.exports = {
   beforeRequest: [...befores],
 
   afterResponse: [...afters],
+
   // If you want your trigger to show up, you better include it here!
   triggers: {},
 
   // If you want your searches to show up, you better include it here!
   searches: {},
+
   // If you want your creates to show up, you better include it here!
   creates: {},
 
